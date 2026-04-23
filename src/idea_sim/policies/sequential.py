ab8387809d14cfc45cf59480ba7f6cdbfd884e4e
Josh McConkie <bjmcconk@byu.edu>
2026-04-23 08:46:00 -0600

    sequential.py: basic implementation finished

--- src/idea_sim/policies/sequential.py
+++ src/idea_sim/policies/sequential.py
@@ -1,20 +1,22 @@
 # Given full preset paths with known utility
-from typing import List, Tuple
+from typing import List, Tuple, Dict
+import numpy as np
 
-def utility_score(choice_set,util_mat) -> Tuple[int,Tuple[int,int]]:
+def utility_score(choice_set,util_mat) -> Tuple[int,List[int,int]]:
     score = 0
     for row in util_mat:
         score += max(row[choice_set])
     return score, choice_set
 
-def best_greedy_choice(options,choice_hist,util_mat):
+def best_greedy_choice(options,choice_hist,util_mat)-> Tuple[int,List[int]]:
+    return max((utility_score(choice_hist+[o],util_mat) for o in options))
 
-    return max((utility_score(choice_hist+o,util_mat) for o in options))
-
-def seq_solve(ord_agents,util_mat):
+def seq_solve(ord_agents: List,
+              agent_choice_dict: Dict[str,List[int]],
+              util_mat: np.ndarray) -> Tuple[int,List[int]]:
     choice_hist: List[int] = []
     for agent in ord_agents:
-        choice_hist.append()
-
-    # for agent in ord_agents:
-    #     score, choice_hist
+        score, choice_hist = best_greedy_choice(agent_choice_dict[agent],
+                                                choice_hist,
+                                                util_mat)
+    return score, choice_hist
