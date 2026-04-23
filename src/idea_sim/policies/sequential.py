a85ef4d00f5d93ed64e7b39ea98b5f81be920c16
Josh McConkie <bjmcconk@byu.edu>
2026-04-23 14:46:04 -0600

    Corrected arg ordering; added debugging statements

--- src/idea_sim/policies/sequential.py
+++ src/idea_sim/policies/sequential.py
@@ -3,12 +3,17 @@ from typing import List, Tuple, Dict
 import numpy as np
 from idea_sim.policies.tools import best_greedy_choice
 
-def seq_solve(ord_agents: List,
-              agent_choice_dict: Dict[str,List[int]],
-              util_mat: np.ndarray) -> Tuple[int,List[int]]:
+def seq_solve(util_mat: np.ndarray,
+            agent_choice_dict: Dict[str,List[int]],
+            ord_agents: List) -> Tuple[int,List[int]]:
+    
+    # print(util_mat)
+    # print(agent_choice_dict)
+    # print(ord_agents)
     choice_hist: List[int] = []
     for agent in ord_agents:
         score, choice_hist = best_greedy_choice(agent_choice_dict[agent],
                                                 choice_hist,
                                                 util_mat)
+    # print(f"Score: {score}")
     return score, choice_hist
