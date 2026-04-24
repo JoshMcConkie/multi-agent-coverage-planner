b4e1fa1e0b55ac0505221fe200fb5eccf1d6673e
Josh McConkie <bjmcconk@byu.edu>
2026-04-24 16:17:54 -0600

    Helper functions to organize solver process. path_model() now returns Model object which is passed to the desired solver separately.
    update_agents() to handle path hist updating. get_all_paths moved from objective.py

--- src/idea_sim/policies/tools.py
+++ src/idea_sim/policies/tools.py
@@ -1,10 +1,16 @@
-from typing import List, Tuple, Dict
-import numpy as np
+from itertools import product
+from operator import add
+from typing import List, Tuple
+from idea_sim.objective import Objective
+from idea_sim.env import GridWorld, Model
 
-'''
 
-'''
 def utility_score(choice_set,util_mat) -> Tuple[int,List[int,int]]:
+    '''Return utility score for given choice set of a utility matrix.
+    This assumes submodularity such that double coverage offers no 
+    bonus to utility.
+
+    '''
     score = 0
     for col in range(util_mat.shape[1]):
         # print(f"util crop: {util_mat[choice_set]}")
@@ -21,3 +27,65 @@ def best_greedy_choice(options: List[int],choice_hist: List[int],util_mat)-> Tup
         util_mat (np.ndarray): The C (aka omega) matrix of allocations and utilities (Nemhauser et al., 1978)
     '''
     return max((utility_score(choice_hist+[o],util_mat) for o in options))
+
+
+def get_all_paths(grid: GridWorld,steps: int,
+                  start_row: Tuple[int,int],start_col:Tuple[int,int]) -> List[List[Tuple[int,int]]]:
+    """Generate all possible path for an agent in the grid.
+
+        #TODO: Avoid other agents starting positions.
+    """
+    if not grid:
+        return None
+    max_row = grid.grid.shape[0]
+    max_col = grid.grid.shape[1]
+    is_in_bounds = lambda rc: 0 <= rc[0] < max_row and 0 <= rc[1] < max_col
+    try:
+        assert is_in_bounds((start_row,start_col))
+    except AssertionError:
+        print("ERROR: Can't get paths. Start position out of bounds.")
+        return None
+        
+    directions = [(-1,0),(0,-1),(0,1),(1,0)]
+    paths = []
+    
+    def step(steps_rem, path):
+        if steps_rem == 0:
+            paths.append(path)
+            return
+        for dir in directions:
+            new_step = tuple(map(add,path[-1],dir))
+            if new_step not in path and is_in_bounds(new_step):
+                step(steps_rem-1,path+[new_step])
+
+    step(steps,[(start_row,start_col)])
+    return paths
+
+def update_agents(model: Model,path_ids: List[int]):
+    '''Append the chosen paths to the agents in the grid.
+    '''
+    for agent in model.grid.agents:
+        agent_path_ids = model.agent_path_dict[agent.id]
+        for path_id in path_ids:
+            if path_id in agent_path_ids:
+                agent.path += model.all_paths[path_id][1:]
+    model.grid.update()
+
+def path_model(grid: GridWorld,objective: Objective, steps: int, agent_order=None):
+    '''Creates object containing grid, utility matrix, available agents path, agent_order
+    (if applicable), and all available paths.
+    '''
+    all_paths = []
+    i = 0
+    agent_path_dict = dict()
+    coordinates = list(product(range(grid.size),range(grid.size)))
+
+    for agent in grid.agents:
+        solutions = get_all_paths(grid,steps,agent.path[-1][0],agent.path[-1][1])
+        # print(f"Agent {agent.id} Solution Sample: {solutions[:5]}")
+        all_paths += solutions
+        agent_path_dict[agent.id] = [i+e for e in range(len(solutions))]
+        i += len(solutions)
+    util_mat = objective.build_util_matrix(all_paths,coordinates) # rows: path idx, cols: coord idx
+    
+    return Model(grid,util_mat,agent_path_dict,agent_order,all_paths)    
