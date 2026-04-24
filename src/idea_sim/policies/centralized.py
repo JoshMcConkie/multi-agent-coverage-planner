f95fcac913dd04ed33be13dc229d843f7440286f
Josh McConkie <bjmcconk@byu.edu>
2026-04-24 16:19:47 -0600

    Update to handle Model object over separate arguments. New helper to separate from sequential solver dependence.

--- src/idea_sim/policies/centralized.py
+++ src/idea_sim/policies/centralized.py
@@ -1,11 +1,28 @@
-from typing import List, Tuple, Dict
-import numpy as np
+from typing import List, Tuple
 from itertools import permutations
-from idea_sim.policies.sequential import seq_solve
+from tqdm import tqdm
+from idea_sim.env import Model
+from idea_sim.policies.tools import update_agents,best_greedy_choice
 
-def cen_solve(util_mat: np.ndarray,
-              agent_choice_dict: Dict[str,List[int]],
-            *_) -> Tuple[int,List[int]]:
-    agent_perm = permutations(agent_choice_dict.keys())
-    return max(seq_solve(util_mat, agent_choice_dict,agent_order) for agent_order in agent_perm)
+def step_cen_solve(model):
+    path_ids: List[int] = []
+    for agent in model.agent_order:
+        score, path_ids = best_greedy_choice(model.agent_path_dict[agent],
+                                                path_ids,
+                                                model.util_mat)
+    return score, path_ids, model.agent_order
+
+
+def cen_solve(model: Model) -> Tuple[int,List[int]]:
+    agent_perm = permutations(model.agent_path_dict.keys())
+    solutions = []
+    og_agent_order = model.agent_order
+    print("Centralized Solver Progress: ")
+    for agent_order in tqdm(agent_perm):
+        model.agent_order = agent_order
+        solutions.append(step_cen_solve(model))
+
+    score,path_ids,model.agent_order = max(solutions)
+    update_agents(model,path_ids)
+    return score, model.agent_order
         
