189e8cbcec32218a7983f93e3649c69307023f37
Josh McConkie <bjmcconk@byu.edu>
2026-04-30 10:00:54 -0600

    Refactored code to use Results class. Refactored seq solvers to use a common helper function.

--- src/idea_sim/policies/sequential.py
+++ src/idea_sim/policies/sequential.py
@@ -4,9 +4,20 @@ from typing import List, Tuple
 import numpy as np
 from idea_sim.policies.tools import best_greedy_choice, path_model, update_agents
 from idea_sim.objective import Objective
-from idea_sim.env import GridWorld, Model
+from idea_sim.env import GridWorld, Model, Result
 
-def seq_solve(model: Model, util_mat_postfix: np.ndarray = None) -> int:
+def choose_seq_paths(model, prior_util_row=None):
+    if model.agent_order is None:
+        raise("ERROR: Sequential solver cannot have model.agent_order == None.")
+    path_ids = []
+    for agent in model.agent_order:
+        score, path_ids = best_greedy_choice(model.agent_path_dict[agent],
+                                                path_ids,
+                                                model.util_mat, prior_util_row)
+    paths_by_agent = update_agents(model,path_ids)
+    return score, path_ids, paths_by_agent
+
+def seq_greedy_solve(model: Model) -> Result:
     '''Sequential submodular maximization solver. Alters in place the attributes of `model`,
     including the attached agents' paths. Returns maximized utility score.
 
@@ -18,45 +29,32 @@ def seq_solve(model: Model, util_mat_postfix: np.ndarray = None) -> int:
 
     returns:
         score: an integer, sequentially maximized objective/utility score.
-        
-    # 
-    
     '''
-    if model.agent_order is None:
-        raise("ERROR: Sequential solver cannot have model.agent_order == None.")
     # print(model.util_mat)
     # print(model.agent_path_dict)
     # print(model.agent_order)
-    path_ids = []
 
-    # handling for split_seq_solve() calls, where a utility rows of
-    # decisions from previous rounds are appended
-    prefix_len = 0
-    if util_mat_postfix is not None:
-        start = model.util_mat.shape[0]
-        prefix_len = util_mat_postfix.shape[0]
-        path_ids = [i + start for i in range(prefix_len)]
-        model.util_mat = np.append(model.util_mat, util_mat_postfix, axis=0)
-
-    for agent in model.agent_order:
-        score, path_ids = best_greedy_choice(model.agent_path_dict[agent],
-                                                path_ids,
-                                                model.util_mat)
-    # print(f"Score: {score}")
-    model.chosen_path_ids = path_ids
-    update_agents(model,path_ids[prefix_len:])
-    return score
+    score, path_ids, paths_by_agent = choose_seq_paths(model)
+    return Result(model.grid.grid,paths_by_agent,
+                  score,seq_greedy_solve,
+                  runtime=None,chosen_path_ids=path_ids,
+                  metadata=None,agent_order=model.agent_order)
 
 
 def split_seq_solve(grid: GridWorld,objective: type[Objective], 
-                    steps: int, agent_order, chunksize: int) -> int:
+                    steps: int, agent_order, chunksize: int) -> Result:
+    
     rounds = [chunksize for s in range(steps // chunksize)] + [steps % chunksize]
-    util_mat_hist = None
-    path_ids_hist = []
+    prior_coverage_state = None
     for chunk in rounds:
         round_model = path_model(grid, objective, chunk, agent_order)
-        seq_solve(round_model,util_mat_hist)
-        util_mat_hist = round_model.util_mat[round_model.chosen_path_ids]
+        score, path_ids, paths_by_agent = choose_seq_paths(round_model,prior_coverage_state)
+        if prior_coverage_state is None:
+            prior_coverage_state = np.zeros(round_model.util_mat.shape[1])
+        prior_coverage_state += round_model.util_mat[path_ids].max(axis=0)
 
     # print(f"Score: {score}")
-    return grid.get_score()
+    return Result(grid.grid,paths_by_agent,
+                  grid.get_score(),seq_greedy_solve,
+                  runtime=None,chosen_path_ids=path_ids,
+                  metadata=None, agent_order=agent_order)
