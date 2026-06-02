82b44bf0afc2ae0391d26d376ed593e25540c828
Josh McConkie <bjmcconk@byu.edu>
2026-06-02 14:07:04 -0600

    brought global vars to central config file

---
+++ src/coverage_planner/experiments/config.py
@@ -0,0 +1,9 @@
+NUM_AGENTS = 7
+MAX_SIZE = 8
+
+SOLVE_OPTIMAL = True
+SWEEP_NAME = "same_start"
+
+# Number of worker processes for the parallel sweep.
+# None defaults to os.cpu_count().
+MAX_WORKERS = None
